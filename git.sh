bd4581bf2df7c9da7f348833727c213c8b2c5128
ccardenas93 <ccardenasf93@gmail.com>
2024-10-28 11:43:01 -0500

    Subida automatica del Mon Oct 28 11:43:01 AM -05 2024

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 cd /home/srvdpahidrologia/Precipitacion_CELEC_html/
 
 cp *.json /home/srvdpahidrologia/celec_precipitacion
-
+cp *.html /home/srvdpahidrologia/celec_precipitacion
 cd /home/srvdpahidrologia/celec_precipitacion
 git add .
 git commit -m "Subida automatica del $(date)"
